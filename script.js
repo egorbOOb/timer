10e7ef72fdebcd0a816523b635114b18c2b0c41f
EgorComputer <egor01.6@inbox.ru>
2020-07-06 02:57:38 +0700

    Урок №19

--- script.js
+++ script.js
@@ -64,5 +64,43 @@ window.addEventListener('DOMContentLoaded', function() {
     }
 }
 
+    // Меню
+
+    const toggleMenu = () => {
+        const btnMenu = document.querySelector('.menu'),
+            menu = document.querySelector('menu'),
+            closeBtn = document.querySelector('.close-btn'),
+            menuItems = menu.querySelectorAll('ul>li');
+
+        const handlerMenu = () => {
+            menu.classList.toggle('active-menu');
+        };
+
+        btnMenu.addEventListener('click', handlerMenu);
+    
+        closeBtn.addEventListener('click', handlerMenu);
+
+        menuItems.forEach((item) => item.addEventListener('click', handlerMenu));
+    };
+
+    //popup
+    const togglePopUp = () => {
+        const popup = document.querySelector('.popup'),
+            popupBtn = document.querySelectorAll('.popup-btn'),
+            popupClose = document.querySelector('.popup-close');
+
+            popupBtn.forEach((elem) => {
+                elem.addEventListener('click', () => {
+                    popup.style.display = 'block';
+                });
+            });
+
+            popupClose.addEventListener('click', () => {
+                popup.style.display = 'none';
+            });
+    };
+
+    togglePopUp();
+    toggleMenu();
     countTimer('5 july 2020');
 });
